459d6aee54e3b591f6c429ea992ba391781246bd
Anni Simonen <annis@Anni-MacBook-Air.local>
2023-01-31 15:01:35 +0200

    backend

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.2xcbpe4.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: personName,
+  number: personNumber
+})
+
+
+if (process.argv.length > 3){
+  person.save().then(
+    console.log('person saved!'),
+    mongoose.connection.close()
+  )
+}
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
